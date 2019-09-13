dd9287a2bb623431ffd40b54c65d3881cc801f8e
Brian Tenggren <btenggren20@gmail.com>
2019-09-13 15:49:14 -0400

    Add user model, spec, and DB stuff

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "throws error when creating user if user not valid" do
+    invalid_attrs = {username: nil, email: nil}
+    user = User.new(invalid_attrs)
+    expect(user.save).to eq false
+  end
+end
